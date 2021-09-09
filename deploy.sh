5c7a5dcf1308e3f3473f5b032949de8e84b703cf
karevalo93 <kevin.arevalo.l@hotmail.com>
2021-09-09 17:03:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t karevalo93/multi-client-k8s:latest -t karevalo93/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t karevalo93/multi-server-k8s:latest -t karevalo93/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
-docker build -t karevalo93/multi-worker-k8s:latest -t karevalo93/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
-docker push karevalo93/multi-client-k8s:latest
-docker push karevalo93/multi-server-k8s:latest
-docker push karevalo93/multi-worker-k8s:latest
+docker build -t karevalo93/multi-client:latest -t karevalo93/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t karevalo93/multi-server:latest -t karevalo93/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t karevalo93/multi-worker:latest -t karevalo93/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push karevalo93/multi-client:latest
+docker push karevalo93/multi-server:latest
+docker push karevalo93/multi-worker:latest
 
-docker push karevalo93/multi-client-k8s:latest:$SHA
-docker push karevalo93/multi-server-k8s:latest:$SHA
-docker push karevalo93/multi-worker-k8s:latest:$SHA
+docker push karevalo93/multi-client:latest:$SHA
+docker push karevalo93/multi-server:latest:$SHA
+docker push karevalo93/multi-worker:latest:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=karevalo93/multi-server-k8s:$SHA
-kubectl set image deployments/client-deployment client=karevalo93/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=karevalo93/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=karevalo93/multi-server:$SHA
+kubectl set image deployments/client-deployment client=karevalo93/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=karevalo93/multi-worker:$SHA
